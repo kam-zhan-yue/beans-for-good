b16999362458d83eb2326fe864961a9b3823e926
kamzhanyue <kamzhanyue@gmail.com>
2024-04-04 13:47:30 +1100

    Init Phaser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
+import PhaserComponent from './components/PhaserComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>My React + Phaser App</h1>
+            </header>
+            <PhaserComponent />
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/PhaserComponent.js
@@ -0,0 +1,46 @@
+// PhaserComponent.js
+import React, { Component } from 'react';
+import Phaser from 'phaser';
+import { Boot } from './scenes/Boot';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        Game,
+        GameOver
+    ]
+};
+
+class PhaserComponent extends Component {
+    
+    componentDidMount() {
+        // Initialize your Phaser game here
+        this.game = new Phaser.Game(config);
+    }
+
+    componentWillUnmount() {
+        // Clean up Phaser resources here
+        this.game.destroy(true);
+    }
+
+    render() {
+        return <div id="game-container" />;
+    }
+}
+
+export default PhaserComponent;
