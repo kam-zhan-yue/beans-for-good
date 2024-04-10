081d49d2c3946e8a08e6c173ce149cb280134d86
kamzhanyue <kamzhanyue@gmail.com>
2024-04-11 00:00:38 +1000

    Fix typo in api

--- frontend/src/StaticHelper.jsx
+++ frontend/src/StaticHelper.jsx
@@ -2,7 +2,7 @@ export default class StaticHelper {
     static getApi() {
         if (process.env.NODE_ENV === 'production') {
             // Production URL
-            return 'https://beans-for-good.onrender.com/';
+            return 'https://beans-for-good-api.onrender.com/';
         } else {
             // Development URL
             return 'http://localhost:3000/';
